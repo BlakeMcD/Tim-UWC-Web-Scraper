64fa117422db370a98dc3895511875a744079483
Blake <blakemcd@gmail.com>
2022-02-16 22:33:06 +1100

    sweet baby Jesus...it works!

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 8000;
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
-const XLSX = require('xlsx');
+const exportUsersToExcel = require('./exportService');
 
 const app = express();
 
@@ -26,6 +26,19 @@ function complete() {
     if (operationsCompleted >= 18) {
         console.log("students length:" + students.length)
         console.log("COMPLETELY COMPLETE!")
+
+        const workSheetColumnName = [
+            "Name", 
+            "Country", 
+            "UWC", 
+            "UNI", 
+            "Class"
+        ]
+
+        const workSheetName = 'Davis Scholar Recipients';
+        const filePath = './outputFiles/excel-from-js.xlsx';
+
+        exportUsersToExcel(students, workSheetColumnName, workSheetName, filePath);
     }
 }
 
@@ -61,7 +74,7 @@ for (let i = 2021; i >= 2018; i--) {
         })
         $('.even', html).each(function(idx, el) {
 
-            const student = { name: "", country: "", uwc: "", uni: ""};
+            const student = { name: "", country: "", uwc: "", uni: "", class: i};
 
             student.name = $(el).children('.views-field-title').text().trim();
             student.country = $(el).children('.views-field-field-country').text().trim();
@@ -113,7 +126,7 @@ for (let i = 2017; i >= 2004; i--) {
         })
         $('.even', html).each(function(idx, el) {
 
-            const student = { name: "", country: "", uwc: "", uni: ""};
+            const student = { name: "", country: "", uwc: "", uni: "", class: i};
 
             student.name = $(el).children('.views-field-title').text().trim();
             student.country = $(el).children('.views-field-field-country').text().trim();
